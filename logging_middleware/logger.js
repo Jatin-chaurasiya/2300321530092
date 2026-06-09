9499da1f906d856fc7d30283ec9cc0d2f272f84d
JATIN KUMAR <jatinkumar638881@gmail.com>
2026-06-09 13:42:27 +0530

    add looger issue

--- logging_middleware/logger.js
+++ logging_middleware/logger.js
@@ -1,10 +1,34 @@
+const BASE_URL =
+  "http://20.244.56.144/evaluation-service/logs";
+
 export async function Log(
   stack,
   level,
   packageName,
-  message
+  message,
+  token
 ) {
-  console.log(
-    `[${stack}] [${level}] [${packageName}] ${message}`
-  );
+  try {
+    const response = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        stack,
+        level,
+        package: packageName,
+        message,
+      }),
+    });
+
+    const data = await response.json();
+
+    console.log("Log Created:", data);
+
+    return data;
+  } catch (error) {
+    console.error("Logger Error:", error);
+  }
 }
